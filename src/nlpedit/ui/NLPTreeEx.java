915bcaa29811e5d0dec12f6f69b13cb806f44747
Boyan Ding <stu_dby@126.com>
2014-05-25 21:03:19 +0800

    clean up unneeded demo; add copyright info

--- src/nlpedit/ui/NLPTreeEx.java
+++ src/nlpedit/ui/NLPTreeEx.java
@@ -1,3 +1,22 @@
+// NLP Editor
+// Copyright (c) 2014	Boyan Ding
+// All rights reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of GNU General Public Licence
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRENTY; without even the implied warrenty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General public license for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+//
+
 package nlpedit.ui;
 
 import java.io.StringReader;
