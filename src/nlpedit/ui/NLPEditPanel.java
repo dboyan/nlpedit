62d517f154ff09d166f9a0a6222cb98269ccf6b4
Boyan Ding <stu_dby@126.com>
2014-06-20 17:19:40 +0800

    Further shaping of the UI

--- src/nlpedit/ui/NLPEditPanel.java
+++ src/nlpedit/ui/NLPEditPanel.java
@@ -22,13 +22,22 @@ package nlpedit.ui;
 import nlpedit.core.NLPProject;
 import java.io.File;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class NLPEditPanel extends JPanel {
 	private NLPProject project;
 	private JTextPane textPane;
+	private JPanel editPanel; // TODO
+	private JLabel statusLabel;
 
 	public NLPEditPanel() {
 		buildGUI();
@@ -37,12 +46,57 @@ public class NLPEditPanel extends JPanel {
 	}
 
 	private void buildGUI() {
+		splitPane = new JSplitPane();
+		topPanel = new JPanel();
+		buttonsPanel = new JPanel();
+		prevButton = new JButton();
+		nextButton = new JButton();
 		textPane = new JTextPane();
+		textScrollPane = new JScrollPane();
+		editPanel = new JPanel(); // TODO
+		statusPanel = new JPanel();
+		statusLabel = new JLabel();
+
+		setLayout(new BorderLayout());
+
+		splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		topPanel.setLayout(new BorderLayout());
+		buttonsPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		prevButton.setText("< Prev");
+		prevButton.setEnabled(false);
+		prevButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				prevActionPerformed(e);
+			}
+		});
+		buttonsPanel.add(prevButton);
+
+		nextButton.setText("Next >");
+		nextButton.setEnabled(false);
+		nextButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				nextActionPerformed(e);
+			}
+		});
+		buttonsPanel.add(nextButton);
+		topPanel.add(buttonsPanel, BorderLayout.NORTH);
 
 		textPane.setEditable(false);
-		textScrollPane = new JScrollPane(textPane);
-		textScrollPane.setPreferredSize(new Dimension(250, 250));
-		add(textScrollPane);
+		textPane.setPreferredSize(new Dimension(250, 250));
+		textScrollPane.setViewportView(textPane);
+		topPanel.add(textScrollPane, BorderLayout.CENTER);
+
+		splitPane.setLeftComponent(topPanel);
+
+		splitPane.setRightComponent(editPanel);
+		add(splitPane, BorderLayout.CENTER);
+
+		statusPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		statusLabel.setText("Ready");
+		statusPanel.add(statusLabel);
+
+		add(statusPanel, BorderLayout.SOUTH);
 	}
 
 	public void importText(File fileName) {
@@ -50,5 +104,17 @@ public class NLPEditPanel extends JPanel {
 		textPane.setText(project.getDocument());
 	}
 
+	private void prevActionPerformed(ActionEvent e) {
+	}
+
+	private void nextActionPerformed(ActionEvent e) {
+	}
+
 	private JScrollPane textScrollPane;
+	private JSplitPane splitPane;
+	private JPanel topPanel;
+	private JPanel buttonsPanel;
+	private JButton prevButton;
+	private JButton nextButton;
+	private JPanel statusPanel;
 }
