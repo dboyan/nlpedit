6a0999af01f2d5fd2877ad11e4515c73f72da8c6
Boyan Ding <stu_dby@126.com>
2014-06-21 21:42:12 +0800

    Create parser event for concurrency

---
+++ src/nlpedit/event/ParseFinishListener.java
@@ -0,0 +1,27 @@
+// NLP Editor
+// Copyright (c) 2014	Boyan Ding
+// All rights reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of GNU General Public Licence
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRENTY; without even the implied warrenty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General public license for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+//
+
+package nlpedit.event;
+
+import java.util.EventListener;
+
+public interface ParseFinishListener extends EventListener {
+	public void parseFinished(ParseFinishEvent e);
+}
+
